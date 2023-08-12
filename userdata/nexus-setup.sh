9f0acae3b42f9534bd4d6cc568133470440bb226
KryCool <43307723+KryCool@users.noreply.github.com>
2023-08-12 20:27:24 +0700

    Update nexus-setup.sh

--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
+yum install java-1.8.0-openjdk.x86_64 wget -y
+mkdir -p /opt/nexus/
+mkdir -p /tmp/nexus/
 cd /tmp/nexus/
 NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
 wget $NEXUSURL -O nexus.tar.gz
@@ -15,24 +15,24 @@ sleep 5
 useradd nexus
 chown -R nexus.nexus /opt/nexus 
 cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
+[Unit]
+Description=nexus service
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=65536
+ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start
+ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop
+User=nexus
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
 
 EOT
 
 echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
 systemctl daemon-reload
-systemctl enable nexus.service
-systemctl start nexus.service
+systemctl enable nexus
+systemctl start nexus
